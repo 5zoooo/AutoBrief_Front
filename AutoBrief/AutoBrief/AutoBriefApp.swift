53a810046ac3dd4b73f1831c63cb0efafe4376e3
김다빈 <mokdb17@gmail.com>
2025-05-19 09:07:18 +0900

    [Setting] Project Setting

---
+++ AutoBrief/AutoBrief/AutoBriefApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoBriefApp.swift
+//  AutoBrief
+//
+//  Created by 김다빈 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AutoBriefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
